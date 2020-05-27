49ada4cc9f06f4af5bd33a677202c1ecc2b7c25d
Silvio Tomatis <silviot@gmail.com>
2020-05-27 12:59:25 +0200

    Move the discovery service to port 80

--- tests/fixtures/complete/settings/base.py
+++ tests/fixtures/complete/settings/base.py
@@ -16,4 +16,4 @@ JWT_AUTH["JWT_SIGNING_ALGORITHM"] = JWT_SIGNING_ALGORITHM
 ECOMMERCE_PUBLIC_URL_ROOT = "http://ecommerce.localhost:4900"
 ECOMMERCE_API_URL = "http://ecommerce.localhost:4900/api/v2"
 
-COURSE_CATALOG_API_URL = "http://discovery.localhost:4910"
+COURSE_CATALOG_API_URL = "http://discovery.discovery-complete.localhost"
